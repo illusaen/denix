dc57cba48fb10595ee4bcfaaabc4429f737f2efe
Wendy Chen <jaewchen@gmail.com>
2026-04-20 12:38:03 -0500

    wip find orphaned preservation folders on /persisted

--- modules/aspects/boot/find-ephemeral.nix
+++ modules/aspects/boot/find-ephemeral.nix
@@ -48,6 +48,69 @@
             "/home/*/.steam"
           ];
 
+          find-orphaned = pkgs.writeShellApplication {
+            name = "fo";
+            runtimeInputs = with pkgs; [
+              fd
+              jq
+            ];
+            text = ''
+              #!${pkgs.runtimeShell}
+              DEPTH="''${1:-3}"
+              PERSIST_DIR="${persistMount}"
+
+              if [[ ! -d $PERSIST_DIR ]]; then
+                exit 0
+              fi
+
+              # shellcheck disable=SC2016
+              dir_filter='
+                def persisted_path:
+                  $mount + .;
+
+                def under($dirs):
+                  . as $path
+                  | any($dirs[]; (. | persisted_path) as $dir | $path == $dir or ($path | startswith($dir + "/")));
+
+                def ancestor_of($paths):
+                  . as $path
+                  | any($paths[]; (. | persisted_path) as $persisted_path | $persisted_path | startswith($path + "/"));
+
+                map(select(
+                  (
+                    under($persisted.directories)
+                    or ancestor_of($persisted.directories)
+                    or ancestor_of($persisted.files)
+                  ) | not
+                )) | .[]
+              '
+
+              # shellcheck disable=SC2016
+              file_filter='
+                def persisted_path:
+                  $mount + .;
+
+                def under($dirs):
+                  . as $path
+                  | any($dirs[]; (. | persisted_path) as $dir | $path == $dir or ($path | startswith($dir + "/")));
+
+                def persisted_file:
+                  . as $path
+                  | any($persisted.files[]; (. | persisted_path) as $file | $path == $file);
+
+                map(select((persisted_file or under($persisted.directories)) | not)) | .[]
+              '
+
+              fd -H -t d -d "$DEPTH" -a . "$PERSIST_DIR" \
+                | jq -R . \
+                | jq -s --argjson persisted '${persisted}' --arg mount "$PERSIST_DIR" "$dir_filter"
+
+              fd -H -t f -d "$DEPTH" -a . "$PERSIST_DIR" \
+                | jq -R . \
+                | jq -s --argjson persisted '${persisted}' --arg mount "$PERSIST_DIR" "$file_filter"
+            '';
+          };
+
           find-ephemeral = pkgs.writeShellApplication {
             name = "ff";
             runtimeInputs = with pkgs; [
@@ -125,6 +188,7 @@
         {
           environment.systemPackages = [
             find-ephemeral
+            find-orphaned
           ];
           environment.etc."persisted-paths.json".text = persisted;
         }
