a7e3fffb2b7b9023b3322b4b34f7a24096b7f707
Wendy Chen <jaewchen@gmail.com>
2026-04-20 18:49:44 -0500

    wip qs

--- modules/aspects/desktop/dms/dms.nix
+++ modules/aspects/desktop/dms/dms.nix
@@ -1,12 +1,8 @@
-{ den, ... }:
+{ den, inputs, ... }:
 {
   flake-file.inputs = {
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     dms-plugin-registry = {
@@ -18,15 +14,20 @@
   den.aspects.desktop.includes = [ den.aspects.dms ];
   den.aspects.dms = den.lib.perHost {
     nixos =
-      { inputs', ... }:
+      { pkgs, ... }:
       {
-        programs.dms-shell = {
+        imports = [ inputs.dms.nixosModules.dank-material-shell ];
+        programs.dank-material-shell = {
           enable = true;
-          package = inputs'.dms.packages.default;
-          quickshell.package = inputs'.quickshell.packages.quickshell;
-
           enableVPN = false;
+          systemd.enable = true;
         };
+
+        environment.systemPackages = with pkgs; [
+          adw-gtk3
+          adwaita-qt6
+          dracula-icon-theme
+        ];
       };
   };
 }
