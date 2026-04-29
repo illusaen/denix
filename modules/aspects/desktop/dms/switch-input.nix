2ef8a9e06927e58145da33fb6b450af03e22617b
Wendy Chen <jaewchen@gmail.com>
2026-04-28 20:49:48 -0500

    fixing switcher to add i2c group correctly

--- modules/aspects/desktop/dms/switch-input.nix
+++ modules/aspects/desktop/dms/switch-input.nix
@@ -1,25 +1,28 @@
 {
-  den.aspects.desktop._.switch-input = {
-    nixos =
-      { pkgs, ... }:
-      let
-        switcher = pkgs.writeShellApplication {
-          name = "switcher";
-          runtimeInputs = with pkgs; [
-            ddcutil
-            i2c-tools
-            python3
-          ];
-          text = ''
-            python3 ${./switch-input.py} "$@"
-          '';
+  den.aspects.desktop._.switch-input =
+    { host }:
+    {
+      nixos =
+        { pkgs, lib, ... }:
+        let
+          switcher = pkgs.writeShellApplication {
+            name = "switcher";
+            runtimeInputs = with pkgs; [
+              ddcutil
+              i2c-tools
+              python3
+            ];
+            text = ''
+              python3 ${./switch-input.py} "$@"
+            '';
+          };
+        in
+        {
+          environment.systemPackages = [ switcher ];
+          hardware.i2c.enable = true;
+          users.users = builtins.listToAttrs (
+            map (username: lib.nameValuePair username { extraGroups = [ "i2c" ]; }) (lib.attrNames host.users)
+          );
         };
-      in
-      {
-        environment.systemPackages = [ switcher ];
-        hardware.i2c.enable = true;
-      };
-
-    user.extraGroups = [ "i2c" ];
-  };
+    };
 }
