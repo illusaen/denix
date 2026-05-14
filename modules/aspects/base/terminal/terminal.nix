b352d06b2fe6295127871351adbed439fb414e1d
Wendy Chen <jaewchen@gmail.com>
2026-05-14 09:19:38 -0500

    WIP route

--- modules/aspects/base/terminal/terminal.nix
+++ modules/aspects/base/terminal/terminal.nix
@@ -1,131 +1,81 @@
-{ den, ... }:
+{
+  den,
+  lib,
+  inputs,
+  ...
+}:
 {
   den.aspects.base.includes = with den.aspects.base; [ terminal ];
 
   den.aspects.base.terminal = {
-    # wrapperPackages =
-    #   { osConfig, ... }:
-    #   {
-    #     kitty =
-    #       {
-    #         wlib,
-    #         lib,
-    #         ...
-    #       }:
-    #       let
-    #         kitty-theme = osConfig.scheme {
-    #           template = ./kitty-theme.conf.mustache;
-    #           extension = "conf";
-    #         };
-    #       in
-    #       {
-    #         imports = [ wlib.wrapperModules.kitty ];
-    #         font = {
-    #           inherit (osConfig.my.fonts.mono) name;
-    #           size = osConfig.my.fonts.sizes.terminal;
-    #         };
-    #         settings = {
-    #           active_border_color = "none";
-    #           background_opacity = 0.6;
-    #           background_blur = 20;
-    #           confirm_os_window_close = 0;
-    #           linux_display_server = "wayland";
-    #           macos_titlebar_color = "system";
-    #           placement_strategy = "bottom-left";
-    #           tab_activity_symbol = "↺";
-    #           tab_bar_margin_height = "0.0 8.0";
-    #           tab_bar_margin_width = "0.0";
-    #           tab_bar_style = "powerline";
-    #           tab_powerline_style = "slanted";
-    #           window_padding_width = 20;
-    #         };
-    #         extraConfig = ''
-    #           # Shell integration is sourced and configured manually
-    #           shell_integration no-rc
-
-    #           include themes/colors.conf
-    #         '';
-    #         constructFiles = {
-    #           "quick-access-terminal.conf" = {
-    #             relPath = "quick-access-terminal.conf";
-    #             content = ''
-    #               hide_on_focus_loss yes
-    #               lines 48
-    #               margin_left 1600
-    #               margin_right 1600
-    #             '';
-    #           };
-    #           "colors.conf" = {
-    #             relPath = "themes/colors.conf";
-    #             builder = ''
-    #               mkdir -p "$(dirname "$2")"
-    #               ln -s ${lib.escapeShellArg kitty-theme} "$2"
-    #             '';
-    #           };
-    #         };
-    #       };
-    #   };
+    wrapper-packages =
+      ctx@{ config, ... }:
+      let
+        osConfig = builtins.trace (lib.attrNames config) config;
+        # osConfig = (if host.class == "darwin" then self.darwinConfigurations else self.nixosConfigurations).${host.name}.config;
+      in
+      builtins.trace (lib.attrNames ctx) {
+        kitty =
+          let
+            kitty-theme = osConfig.scheme {
+              template = ./kitty-theme.conf.mustache;
+              extension = "conf";
+            };
+            inherit (osConfig.my) fonts;
+          in
+          {
+            imports = [ inputs.wrappers.lib.wrapperModules.kitty ];
+            font = {
+              inherit (fonts.mono) name;
+              size = fonts.sizes.terminal;
+            };
+            settings = {
+              active_border_color = "none";
+              background_opacity = 0.6;
+              background_blur = 20;
+              confirm_os_window_close = 0;
+              linux_display_server = "wayland";
+              macos_titlebar_color = "system";
+              placement_strategy = "bottom-left";
+              tab_activity_symbol = "↺";
+              tab_bar_margin_height = "0.0 8.0";
+              tab_bar_margin_width = "0.0";
+              tab_bar_style = "powerline";
+              tab_powerline_style = "slanted";
+              window_padding_width = 20;
+            };
+            themeFile = "cosmic";
+            extraConfig = ''
+              # Shell integration is sourced and configured manually
+              shell_integration no-rc
+            '';
+            constructFiles = {
+              "quick-access-terminal.conf" = {
+                relPath = "quick-access-terminal.conf";
+                content = ''
+                  hide_on_focus_loss yes
+                  lines 48
+                  margin_left 1600
+                  margin_right 1600
+                '';
+              };
+              "cosmic.conf" = {
+                relPath = "kitty-themes/colors.conf";
+                builder = ''
+                  mkdir -p "$(dirname "$2")"
+                  ln -s ${lib.escapeShellArg kitty-theme} "$2"
+                '';
+              };
+            };
+          };
+      };
 
     nixos =
-      { config, pkgs, ... }:
-      let
-        kitty-theme = config.scheme {
-          template = ./kitty-theme.conf.mustache;
-          extension = "conf";
-        };
-        kitty-settings = pkgs.writeText "kitty.conf" ''
-          font_family ${config.my.fonts.mono.name}
-          font_size ${toString config.my.fonts.sizes.terminal}
-
-          # Shell integration is sourced and configured manually
-          shell_integration no-rc
-
-          active_border_color none
-          background_opacity 0.6
-          background_blur 20
-          confirm_os_window_close 0
-          linux_display_server wayland
-          macos_titlebar_color system
-          placement_strategy bottom-left
-          tab_activity_symbol ↺
-          tab_bar_margin_height 0.0 8.0
-          tab_bar_margin_width 0.0
-          tab_bar_style powerline
-          tab_powerline_style slanted
-          window_padding_width 20
-
-          include themes/colors.conf
-        '';
-        kitty-quick-settings = pkgs.writeText "quick-access-terminal.conf" ''
-          hide_on_focus_loss yes
-          lines 48
-          margin_left 1600
-          margin_right 1600
-        '';
-
-        kitty-wrapped = pkgs.symlinkJoin {
-          name = "kitty";
-          paths = [ pkgs.kitty ];
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            mkdir -p $out/themes
-            install -Dm644 ${kitty-settings} $out/kitty.conf
-            install -Dm644 ${kitty-quick-settings} $out/quick-access-terminal.conf
-            install -Dm644 ${kitty-theme} $out/themes/colors.conf
-            wrapProgram $out/bin/kitty --add-flag --config --add-flag $out/kitty.conf --add-flag --themes-dir --add-flag $out/themes --set KITTY_CONFIG_DIRECTORY $out
-            wrapProgram $out/bin/kitten --set KITTY_CONFIG_DIRECTORY $out
-          '';
-        };
-      in
+      { self', ... }:
       {
-        environment.systemPackages = with pkgs; [
-          # self'.packages.kitty
-          ghostty
-          kitty-wrapped
+        environment.systemPackages = [
+          self'.packages.kitty
         ];
-        systemd.packages = [ pkgs.ghostty ];
-        services.dbus.packages = [ pkgs.ghostty ];
-
       };
 
     darwin =
--- modules/flake/wrappers.nix
+++ modules/flake/wrappers.nix
@@ -2,6 +2,7 @@
   inputs,
   self,
   den,
+  lib,
   ...
 }:
 {
@@ -13,17 +14,45 @@
   imports = [ inputs.wrappers.flakeModules.wrappers ];
   flake.nixosModules = builtins.mapAttrs (_: v: v.install) self.wrappers;
 
-  den.classes.wrappers.description = "Wrapper class for nix-wrapper-modules";
-  den.policies.wrappers-to-flake =
+  den.classes.flake-config.description = "Flake-level configuration";
+  den.policies.flake-config-to-flake =
+    { system, ... }@ctx:
+    builtins.trace ctx lib.optional (system == builtins.currentSystem) (
+      den.lib.policy.route {
+        fromClass = "flake-config";
+        intoClass = "flake";
+        path = [ "flake" ];
+        # guard = { host, ... }: host.class == class;
+        instantiate =
+          { modules, ... }:
+          removeAttrs
+            (lib.evalModules {
+              modules = [
+                { config._module.freeformType = lib.types.lazyAttrsOf lib.types.raw; }
+              ]
+              ++ modules;
+            }).config
+            [ "_module" ];
+      }
+    );
+  den.schema.flake-system.includes = [ den.policies.flake-config-to-flake ];
+
+  den.quirks.wrapper-packages.description = "Wrapper class for nix-wrapper-modules";
+  den.policies.collect-wrapper-packages =
     _:
-    (den.lib.policy.route {
-      fromClass = "wrapperPackages";
-      intoClass = "flake";
-      path = [
-        "flake"
-        "wrappers"
-      ];
-      adaptArgs = args: args // { osConfig = args.config; };
-    });
-  den.schema.host.includes = [ den.policies.wrappers-to-flake ];
+    let
+      inherit (den.lib.policy) pipe;
+    in
+    [
+      (pipe.from "wrapper-packages" [
+        (pipe.collect (_: true))
+      ])
+    ];
+  den.schema.host.includes = [ den.policies.collect-wrapper-packages ];
+
+  den.default.flake-config =
+    { wrapper-packages, ... }:
+    {
+      wrappers = builtins.foldl' lib.recursiveUpdate { } wrapper-packages;
+    };
 }
