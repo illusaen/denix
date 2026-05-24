8a0a7be4ed46ebec2159386d5cf6e990f3811cdb
Wendy Chen <jaewchen@gmail.com>
2026-05-23 19:15:39 -0500

    Removing lix, slower than cppnix.

--- modules/aspects/base/nix-config.nix
+++ modules/aspects/base/nix-config.nix
@@ -12,48 +12,33 @@
       programs.nix-ld.enable = true;
     };
 
-    os =
-      { pkgs, ... }:
-      {
-        nix.settings = {
-          experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
-          warn-dirty = false;
-          trusted-users = [ "@wheel" ];
-          substituters = [
-            "https://cache.nixos.org/"
-            "https://nix-community.cachix.org"
-            "https://nixpkgs-unfree.cachix.org"
-          ];
-          trusted-public-keys = [
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-          ];
-        };
-
-        nixpkgs.config.allowUnfree = true;
-
-        time.timeZone = "America/Chicago";
-
-        security.sudo.extraConfig = ''
-          Defaults lecture = never
-        '';
-
-        nixpkgs.overlays = [
-          (_final: prev: {
-            inherit (prev.lixPackageSets.latest)
-              nixpkgs-review
-              nix-eval-jobs
-              nix-fast-build
-              colmena
-              ;
-          })
+    os = {
+      nix.settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
         ];
-
-        nix.package = pkgs.lixPackageSets.stable.lix;
+        warn-dirty = false;
+        trusted-users = [ "@wheel" ];
+        substituters = [
+          "https://cache.nixos.org/"
+          "https://nix-community.cachix.org"
+          "https://nixpkgs-unfree.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        ];
       };
+
+      nixpkgs.config.allowUnfree = true;
+
+      time.timeZone = "America/Chicago";
+
+      security.sudo.extraConfig = ''
+        Defaults lecture = never
+      '';
+    };
   };
 }
