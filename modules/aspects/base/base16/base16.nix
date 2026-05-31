f8bb9700dd14b4d1ae4be5d9dd22e1583fb9e60c
Wendy Chen <jaewchen@gmail.com>
2026-05-30 22:06:45 -0500

    Using base16's prewritten library code instead of manually parsing

--- modules/aspects/base/base16/base16.nix
+++ modules/aspects/base/base16/base16.nix
@@ -5,47 +5,11 @@
 }:
 let
   base16Scheme = ./cosmic.yaml;
-  base16Msg = import (inputs.base16 + "/lib/msg.nix");
-  mkFromYaml = import (inputs.base16.inputs.fromYaml + "/fromYaml.nix") { inherit lib; };
-  mkBase16Util =
-    {
-      pkgs ? null,
-    }:
-    import (inputs.base16 + "/lib/util.nix") {
-      inherit lib pkgs;
-      msg = base16Msg;
-      fromYaml = mkFromYaml;
-    };
-  colors =
-    let
-      util = mkBase16Util { };
-    in
-    import (inputs.base16 + "/lib/colors.nix") {
-      inherit lib;
-      inherit (util) normalize-colors;
-    };
-  fromYaml = mkFromYaml;
-  util = mkBase16Util { };
-  parsedScheme = util.convert-scheme-to-common-format (fromYaml (builtins.readFile base16Scheme));
-  inputAttrs = colors.colors parsedScheme;
-  inputMeta = rec {
-    scheme = "${inputAttrs.scheme or "untitled"}";
-    author = "${inputAttrs.author or "untitled"}";
-    description = "${inputAttrs.description or scheme}";
-    variant = "${inputAttrs.variant or "unspecified"}";
-    system = "${inputAttrs.system or "base16"}";
-    slug = lib.toLower (lib.strings.sanitizeDerivationName (inputAttrs.slug or scheme));
+  base16 = inputs.base16.lib {
+    inherit lib;
+    pkgs = null;
   };
-  builderMeta = {
-    scheme-name = inputMeta.scheme;
-    scheme-author = inputMeta.author;
-    scheme-description = inputMeta.description;
-    scheme-slug = inputMeta.slug;
-    scheme-slug-underscored = builtins.replaceStrings [ "-" ] [ "_" ] inputMeta.slug;
-    scheme-system = inputMeta.system;
-    scheme-variant = inputMeta.variant;
-  };
-  schemeAttrs = inputAttrs // inputMeta // builderMeta;
+  schemeAttrs = base16.mkSchemeAttrs base16Scheme;
 in
 {
   options.fleet.my = {
@@ -71,7 +35,6 @@ in
     fleet.my = {
       base16.colorScheme = "dark";
       scheme = schemeAttrs // {
-        withHashtag = schemeAttrs.withHashtag // inputMeta // builderMeta;
         render =
           {
             pkgs,
@@ -79,26 +42,14 @@ in
             ...
           }@args:
           let
-            renderUtil = mkBase16Util { inherit pkgs; };
-            mkTheme = import (inputs.base16 + "/lib/mk-theme.nix") (
-              {
-                inherit lib pkgs;
-                msg = base16Msg;
-                fromYaml = mkFromYaml;
-              }
-              // renderUtil
-            );
+            renderBase16 = inputs.base16.lib { inherit lib pkgs; };
+            renderScheme = renderBase16.mkSchemeAttrs base16Scheme;
           in
-          mkTheme.mkTheme (
-            (removeAttrs args [
+          renderScheme (
+            removeAttrs args [
               "lib"
               "pkgs"
-            ])
-            // {
-              scheme = schemeAttrs // {
-                withHashtag = schemeAttrs.withHashtag // inputMeta // builderMeta;
-              };
-            }
+            ]
           );
       };
     };
