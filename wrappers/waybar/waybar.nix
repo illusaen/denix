dc528786f73513396b9cca275b6bd29ec9b1220d
Wendy Chen <jaewchen@gmail.com>
2026-06-13 11:12:10 -0500

    fixed waybar

--- wrappers/waybar/waybar.nix
+++ wrappers/waybar/waybar.nix
@@ -24,100 +24,86 @@
     };
   };
 
-  config.settings = {
-    layer = "top";
-    position = "right";
-    width = 56;
-    margin-top = 16;
-    margin-bottom = 16;
-    margin-left = 8;
-    margin-right = 8;
-    spacing = 16;
-    modules-left = [
-      "niri/workspaces"
-    ];
-    modules-center = [ "group/date-time" ];
-    modules-right = [
-      "tray"
-      "custom/notification"
-    ];
-    "niri/workspaces" = {
-      format = "{icon}";
-      format-icons = {
-        active = "●";
-        default = "○";
+  config.settings =
+    let
+      inherit (config.font) sans mono;
+      clockSegment = font: size: text: {
+        interval = 60;
+        format = "<span font_family='${font}' size='${size}pt'>{0:${text}}</span>";
+        tooltip-format = "<span font_family='${mono}' size='large'>{calendar}</span>";
+        calendar.format.months = "<span color='${config.scheme.base09}'><b>{}</b></span>";
+        calendar.format.today = "<span color='${config.scheme.base09}'><b>{}</b></span>";
       };
-    };
-    "group/date-time" = {
-      orientation = "inherit";
-      modules = [
-        "clock#hour"
-        "clock#minute"
-        "clock#period"
-        "clock#weekday"
-        "clock#month"
-        "clock#day"
+    in
+    {
+      layer = "top";
+      position = "right";
+      width = 56;
+      margin-top = 16;
+      margin-bottom = 16;
+      margin-left = 8;
+      margin-right = 8;
+      spacing = 16;
+      modules-left = [
+        "niri/workspaces"
       ];
-    };
-    "clock#hour" = {
-      interval = 60;
-      format = "<span font_family='${config.font.mono}' size='15pt'>{0:%I}</span>";
-      tooltip = false;
-    };
-    "clock#minute" = {
-      interval = 60;
-      format = "<span font_family='${config.font.mono}' size='15pt'>{0:%M}</span>";
-      tooltip = false;
-    };
-    "clock#period" = {
-      interval = 60;
-      format = "<span font_family='${config.font.mono}' size='12pt'>{0:%p}</span>";
-      tooltip = false;
-    };
-    "clock#weekday" = {
-      interval = 60;
-      format = "<span font_family='${config.font.sans}' size='12pt'>{0:%a}</span>";
-      tooltip = false;
-    };
-    "clock#month" = {
-      interval = 60;
-      format = "<span font_family='${config.font.sans}' size='12pt'>{0:%b}</span>";
-      tooltip = false;
-    };
-    "clock#day" = {
-      interval = 60;
-      format = "<span font_family='${config.font.mono}' size='15pt'>{0:%d}</span>";
-      tooltip-format = "<span font_family='${config.font.mono}' size='large'>{calendar}</span>";
-      calendar.format.today = "<span color='${config.scheme.base09}'><b>{}</b></span>";
-    };
-    tray = {
-      icon-size = 20;
-      spacing = 8;
-    };
-    "custom/notification" =
-      let
-        swaync-client = lib.getExe' pkgs.swaynotificationcenter "swaync-client";
-      in
-      {
-        exec = "${swaync-client} -swb";
-        exec-if = "which ${swaync-client}";
-        restart-interval = 5;
-        return-type = "json";
+      modules-center = [ "group/date-time" ];
+      modules-right = [
+        "tray"
+        "custom/notification"
+      ];
+      "niri/workspaces" = {
         format = "{icon}";
         format-icons = {
-          notification = "notifications";
-          none = "notifications_none";
-          dnd-notification = "notifications_off";
-          dnd-none = "notifications_off";
-          inhibited-notification = "notifications_off";
-          inhibited-none = "notifications_off";
-          dnd-inhibited-notification = "notifications_off";
-          dnd-inhibited-none = "notifications_off";
+          active = "●";
+          default = "○";
         };
-        on-click = "${lib.getExe' pkgs.swaynotificationcenter "swaync-client"} -t -sw";
-        on-click-right = "rofi-notifications";
       };
-  };
+      "group/date-time" = {
+        orientation = "inherit";
+        modules = [
+          "clock#hour"
+          "clock#minute"
+          "clock#period"
+          "clock#weekday"
+          "clock#month"
+          "clock#day"
+        ];
+      };
+      "clock#hour" = clockSegment mono "15" "%I";
+      "clock#minute" = clockSegment mono "15" "%M";
+      "clock#period" = clockSegment mono "12" "%p";
+      "clock#weekday" = clockSegment sans "12" "%a";
+      "clock#month" = clockSegment sans "12" "%b";
+      "clock#day" = clockSegment mono "15" "%d";
+      tray = {
+        icon-size = 20;
+        spacing = 8;
+      };
+      "custom/notification" =
+        let
+          swaync-client = lib.getExe' pkgs.swaynotificationcenter "swaync-client";
+        in
+        {
+          exec = "${swaync-client} -swb";
+          exec-if = "which ${swaync-client}";
+          restart-interval = 5;
+          return-type = "json";
+          format = "{icon}";
+          format-icons = {
+            notification = "notifications";
+            none = "notifications_none";
+            dnd-notification = "notifications_off";
+            dnd-none = "notifications_off";
+            inhibited-notification = "notifications_off";
+            inhibited-none = "notifications_off";
+            dnd-inhibited-notification = "notifications_off";
+            dnd-inhibited-none = "notifications_off";
+          };
+          on-click = "${lib.getExe' pkgs.swaynotificationcenter "swaync-client"} -t -sw";
+          on-click-right = "rofi-notifications";
+        };
+    };
   config."style.css".path = pkgs.replaceVars ./style.css {
     inherit (config.scheme)
       base00
